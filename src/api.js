a1727070eb2206ed8abd602672d2a50bf9af5a32
Tatiana Inama <tatii.oxo@gmail.com>
2021-11-23 18:34:02 +0100

    Mock data only on MOCK mode

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API = import.meta.env.VITE_TWITCH_API;
 
-const MOCK_DATA = import.meta.env.DEV ? [
+const MOCK_DATA = import.meta.env.MODE === 'MOCK' ? [
   { viewers: 7509, user: "Vixella", desc: "villager hunting day 2 (๑✧◡✧๑) !bingo", category: "Animal Crossing: New Horizons", avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/8fccb72a-a9d3-43c9-90fc-a90a54284ae7-profile_image-70x70.png"},
   { viewers: 10000, user: "lilsimsie", desc: "building a restaurant for henford-on-bagle", category: "The Sims 4", avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/81f1a47f-0f18-4276-80e6-568aef8715e0-profile_image-70x70.png" },
   { viewers: 250, user: "tokidokitraveller", desc: "In some hotel in Wakayama", category: "Just Chatting", avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/6cbd36d7-8851-490b-a299-cf525763e41c-profile_image-70x70.png" },
